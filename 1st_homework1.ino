052cc2c94e758a56ac7c1cf8e5315c2d1e6d845b
RTheKitsune <rahuerta023@gmail.com>
2025-05-03 19:40:21 -0600

    Add files via upload

---
+++ 1st_homework1.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop()
+{
+ Serial.println("Hello world,this is my homework");
+  delay(3000);//This is a comment and works as indicator
+  
+ Serial.println("And this is awesome");
+  delay(3000);
+  
+ Serial.println("Actually Im enjoying this");
+  delay(3000);
+  
+  
+}
